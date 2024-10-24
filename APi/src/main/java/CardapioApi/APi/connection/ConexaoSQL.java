936f76719cf240b6a19f52300f6aabcfa426713a
Gabriel de Castro <108163958+kkgabrielcas@users.noreply.github.com>
2024-10-24 12:31:42 -0300

    Update ConexaoSQL.java

--- APi/src/main/java/CardapioApi/APi/connection/ConexaoSQL.java
+++ APi/src/main/java/CardapioApi/APi/connection/ConexaoSQL.java
@@ -8,7 +8,7 @@ public class ConexaoSQL {
 
     private static final String URL = "jdbc:mysql://localhost:3306/restaurante";
     private static final String USER = "root";
-    private static final String PASSWORD = "mood";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
